e6326adf935f0b022977ab9f4cdd553fc468db9e
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-08-28 21:53:57 -0400

    Update array_functions.php

--- array_functions.php
+++ array_functions.php
@@ -27,9 +27,17 @@ Array
                     [0] => Ferrari
                     [1] => Porsche
                 )
-
         )
-
 )
-
 */
+
+// The array_slice() returns the sequence of elements from the array array as specified by the offset and length parameters.
+$pasta = array("spaghetti", "penne", "maracorni", "farfalle", "orecchiette");
+
+print_r(array_slice($pasta, 2)); // Output: Array ( [0] => maracorni [1] => farfalle [2] => orecchiette ) 
+print_r(array_slice($pasta, 2, -1)); // Output: Array ( [0] => maracorni [1] => farfalle )
+print_r(array_slice($pasta, 2, -1, true)); // Output: // Array ( [2] => maracorni [3] => farfalle )
+print_r(array_slice($pasta, 1)); // Array ( [0] => penne [1] => maracorni [2] => farfalle [3] => orecchiette )
+print_r(array_slice($pasta, -1)); // Array ( [0] => orecchiette )
+
+
